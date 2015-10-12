deaebe70dd23f240cf820c78803fff10f953db4f
david <david@happy-dev.fr>
2015-10-12 18:14:57 +0200

    boucle

--- scrapytuto/spiders/who_spider.py
+++ scrapytuto/spiders/who_spider.py
@@ -12,13 +12,58 @@ class WhoSpider(scrapy.Spider):
     #"http://apps.who.int/gho/data/node.home/"
     ]
 
-    def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        print response.url.split("/")
-        with open(filename, 'wb') as f:
-            f.write(response.body)
 
+    def parse(self, response):
         print '----------- parse dataset /ROOT -------------'
+        print response.meta
+        # if not 'dataset_root' in response.meta['dataset_root']:
+        #     response.meta['dataset_root'] = ""
+
+        sel_iframe = response.xpath("//iframe").extract()
+        print sel_iframe, len(sel_iframe)
+        # if sel_iframe:
+        #     print sel_iframe.extact()
+        #     return
+        for sel in response.xpath("//ul[contains(@class,'list_dash')]/li"):
+            title = sel.xpath('a/text()').extract()
+            link = sel.xpath('a/@href').extract()
+            desc = sel.xpath('text()').extract()
+
+            print '******************************'
+            print link[0]
+            print title[0]
+            # url = response.xpath(link)
+            # url = response.urljoin(link)
+            # print url
+            if link[0]:
+                print '----------- dataset_root -------------'
+                # if 'dataset_root' in response.meta:
+                #     print response.meta['dataset_root'][0]+title[0]
+                # else:
+                #     dataset_root = title[0]
+                dataset_root = title[0]
+                url = "http://apps.who.int/gho/data/"+link[0];
+                yield scrapy.Request(url, callback=self.parse, meta={'url': url, 'dataset_root': dataset_root})
+
+            else:
+                for sel in response.xpath("//ul[contains(@class,'list_dash')]/li"):
+                    title = sel.xpath('a/text()').extract()
+                    link = sel.xpath('a/@href').extract()
+                    desc = sel.xpath('text()').extract()
+                    print 'resources  ******************************'
+                    print link[0]
+                    print title[0]
+            break
+
+    def parse_dir_contents(self, response):
+
+        print '----------- parse dataset /child/dataset_root -------------'
+        print response.meta['dataset_root']
+        print response.meta['url']
+        print '----------- parse dataset /child/iframe -------------'
+        sel_iframe = response.xpath("//iframe").extract()
+        print sel_iframe
+        print '----------- parse dataset /child/... -------------'
         for sel in response.xpath("//ul[contains(@class,'list_dash')]/li"):
             title = sel.xpath('a/text()').extract()
             link = sel.xpath('a/@href').extract()
